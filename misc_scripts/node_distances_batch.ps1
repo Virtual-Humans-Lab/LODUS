d7ded84ee5d3949cc39d138fa4f0421a6601bb87
GabrielFSilva <gabrielsilva.prog@gmail.com>
2023-04-12 12:11:42 -0300

    Levy Parameter Test Experiment Batch

--- misc_scripts/node_distances_batch.ps1
+++ misc_scripts/node_distances_batch.ps1
@@ -1,10 +1,14 @@
-python .\node_distances.py --e NodeDistances13 --y 2000
+<#python .\node_distances.py --e NodeDistances13 --y 2000
 python .\node_distances.py --e NodeDistances13 --d 2 --b 500 --y 2000
 python .\node_distances.py --e NodeDistances13 --d 3 --b 500 --y 2000
 python .\node_distances.py --e NodeDistances13 --d 3 --b 250 --y 1000
 
 python .\node_distances.py --e NodeDistances94 --y 16000
 python .\node_distances.py --e NodeDistances94 --d 2 --b 500 --y 16000
-python .\node_distances.py --e NodeDistances94 --d 3 --b 500 --y 16000
+python .\node_distances.py --e NodeDistances94 --d 3 --b 500 --y 16000#>
+
+python .\node_distances.py --e NodeDistances94 --d 3 --b 250 --y 30000
+python .\node_distances.py --e NodeDistances94 --d 3 --b 500 --y 30000
+python .\node_distances.py --e NodeDistances94 --d 3 --b 1000 --y 30000
 
 Read-Host -Prompt "Press Enter to continue"
